5bda2c02b3de3f48747f422dc20b883f03e3b7f2
Dhananjhay <dhananjhay03@gmail.com>
2024-08-29 11:19:57 -0400

    updated docstring

--- autoafids_prep/workflow/scripts/n4_bias_corr.py
+++ autoafids_prep/workflow/scripts/n4_bias_corr.py
@@ -2,7 +2,23 @@
 import SimpleITK as sitk
 import sys
 
+
 def n4_bias_correction(input_image_path, output_image_path):
+    """
+    Performs n4 bias field correction.
+
+    Parameters
+    ----------
+        input_image_path:: str
+            Path to the input image
+
+        output_image_path:: str
+            Path to the output image
+
+    Returns
+    -------
+        None
+    """
     input_image = sitk.ReadImage(input_image_path, sitk.sitkFloat32)
 
     corrector = sitk.N4BiasFieldCorrectionImageFilter()
@@ -12,11 +28,13 @@ def n4_bias_correction(input_image_path, output_image_path):
 
     sitk.WriteImage(output_image, output_image_path)
 
+
 def main(snakemake):
     input_image_path = snakemake.input.im
     output_image_path = snakemake.output.corrected_im
-    
+
     n4_bias_correction(input_image_path, output_image_path)
+
 
 if __name__ == "__main__":
     main(snakemake)
--- autoafids_prep/workflow/scripts/resample_img.py
+++ autoafids_prep/workflow/scripts/resample_img.py
@@ -5,37 +5,50 @@ from nilearn.image import resample_img
 import numpy as np
 import nibabel as nib
 
-def resample_to_isotropic_resolution(nifti_image_path, isotropic_resolution, output_img):
+
+def resample_to_isotropic_resolution(
+    nifti_image_path, isotropic_resolution, output_img
+):
     """
     Resample a NIfTI MRI image to the specified isotropic resolution.
 
-    Parameters:
-    - nifti_image_path: str, path to the NIfTI MRI image file.
-    - isotropic_resolution: float, the desired isotropic resolution in mm.
+    Parameters
+    ----------
+        nifti_image_path:: str
+            Path to the NIfTI MRI image file.
 
-    Returns:
-    - nib.Nifti1Image: The resampled NIfTI image.
+        isotropic_resolution:: float
+            The desired isotropic resolution in mm.
+
+        output_img :: str
+            Name of the modified image
+
+    Returns
+    -------
+        None
+
     """
     # Load the NIfTI image
     img = nib.load(nifti_image_path)
 
     # Get the original affine
-    original_affine = np.eye(3,3)
+    original_affine = np.eye(3, 3)
 
     # Create the target affine with the desired isotropic resolution
     target_affine = np.copy(original_affine)
-    np.fill_diagonal(target_affine, isotropic_resolution/100)
+    np.fill_diagonal(target_affine, isotropic_resolution / 100)
 
     # Resample the image
-    resampled_image = resample_img(img,
-                                 target_affine=target_affine,
-                                 interpolation='continuous')
-    
+    resampled_image = resample_img(
+        img, target_affine=target_affine, interpolation="continuous"
+    )
+
     resampled_image.to_filename(output_img)
+
 
 if __name__ == "__main__":
     resample_to_isotropic_resolution(
         nifti_image_path=snakemake.input["im_normed"],
-        isotropic_resolution= snakemake.params["res"],
+        isotropic_resolution=snakemake.params["res"],
         output_img=snakemake.output["resam_im"],
     )
